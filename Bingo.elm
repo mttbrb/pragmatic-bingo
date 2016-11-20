ed95856d8427f48dfb098874419adebc7da1f757
matt berube <mttbrb@gmail.com>
2016-11-19 23:06:35 -0500

    through lesson 6

--- Bingo.elm
+++ Bingo.elm
@@ -1,7 +1,47 @@
 module Bingo exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+playerInfo : String -> Int -> String
+playerInfo name gameNumber =
+  name ++ " - Game #" ++ (toString gameNumber)
+
+
+shout : String -> String
+shout = String.toUpper
+
+
+viewPlayer : String -> Int -> Html msg
+viewPlayer name gameNumber =
+  let playerInfoText =
+      playerInfo name gameNumber
+      |> shout
+      |> text
+  in
+    h2 [ id "info", class "classy"] [ playerInfoText ]
+
+
+viewHeader : String -> Html msg
+viewHeader title =
+  header [] [ h1 [] [ text title ]]
+
+
+viewFooter : Html msg
+viewFooter =
+  footer [] [ a [ href "http://elm-lang.org" ]
+                [ text "Powered by Elm" ]
+            ]
+
+view : Html msg
+view =
+  div [ class "content" ]
+    [ viewHeader "BUZZWORD BINGO"
+    , viewPlayer "Missy" 5
+    , viewFooter
+    ]
 
 main: Html.Html msg
 main =
-  Html.text "Hello ELM"
+  view
